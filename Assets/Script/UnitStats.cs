bf241e7b9df134c2e052dc64861e307ca5c2ab26
Brunosaur <mr.bruno.bouchard@gmail.com>
2018-01-15 21:19:12 -0500

    État Initial

---
+++ Assets/Script/UnitStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UnitStats {
+	public string Name;
+	public UnitClass Class;
+	public float Health;
+	public float Damage;
+	public string Other;
+	//
+	[HideInInspector]
+	public int Kills;
+}
---
+++ Assets/Script/UnitStatsLogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum UnitClass {Offensive, Defensive, Support};
+
+public class UnitStatsLogic : MonoBehaviour {
+
+	public UnitStats StatsSheet;
+
+	// Use this for initialization
+	void Start () {
+		WriteStats ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void WriteStats ()
+	{
+		Transform unitCanvas = transform.GetChild (0);
+		unitCanvas.GetChild (0).GetComponent<Text> ().text = StatsSheet.Name;
+		unitCanvas.GetChild (1).GetComponent<Text> ().text = StatsSheet.Class.ToString();
+		unitCanvas.GetChild (2).GetComponent<Text> ().text = StatsSheet.Health.ToString();
+		unitCanvas.GetChild (3).GetComponent<Text> ().text = StatsSheet.Damage.ToString();
+		unitCanvas.GetChild (4).GetComponent<Text> ().text = StatsSheet.Kills.ToString();
+	}
+}
